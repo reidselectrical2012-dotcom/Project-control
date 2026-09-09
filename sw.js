5b77c9e0458c6dc757d179bd103e1df827ff4b33
reidselectrical2012-dotcom <reidselectrical2012@gmail.com>
2026-09-09 23:02:52 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='project-control-v14';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
