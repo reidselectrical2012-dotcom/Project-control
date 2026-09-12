0d025746261b7a80b8cbd5bb8230052d71a2ff30
reidselectrical2012-dotcom <reidselectrical2012@gmail.com>
2026-09-12 18:59:26 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='clickgrid-ppm-v35';
+self.addEventListener('install',event=>{self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));});
